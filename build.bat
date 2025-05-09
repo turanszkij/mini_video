86a1358f772e9b6bca69d47e9f2681b15a1c35a0
Turánszki János <turanszkij@users.noreply.github.com>
2025-05-09 17:28:01 +0200

    support c++ 14

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ for /f "usebackq tokens=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Stu
 call "%VS_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x64
 echo LIB=%LIB%
 
-cl.exe /permissive- /ifcOutput /GS /GL /W3 /Gy /Zc:wchar_t /Zc:inline /fp:precise /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /Oi /MT /std:c++17 /FC /EHsc /nologo mini_video_dx12.cpp /link user32.lib kernel32.lib
+cl.exe /permissive- /ifcOutput /GS /GL /W3 /Gy /Zc:wchar_t /Zc:inline /fp:precise /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /Oi /MT /FC /EHsc /nologo mini_video_dx12.cpp /link user32.lib kernel32.lib
 
-cl.exe /permissive- /ifcOutput /GS /GL /W3 /Gy /Zc:wchar_t /Zc:inline /fp:precise /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /Oi /MT /std:c++17 /FC /EHsc /nologo mini_video_vulkan.cpp /link user32.lib kernel32.lib
+cl.exe /permissive- /ifcOutput /GS /GL /W3 /Gy /Zc:wchar_t /Zc:inline /fp:precise /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /Oi /MT /FC /EHsc /nologo mini_video_vulkan.cpp /link user32.lib kernel32.lib
